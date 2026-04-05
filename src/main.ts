53a2effeed09038a35a1863e493f0148f5ecac66
y-v-h <gyrinovich@gmail.com>
2026-04-06 00:30:24 +0200

    feat: add useGlobalInterceptors

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   const config = new DocumentBuilder()
-  .setTitle('Knowledge Hub')
-  .setDescription(' Knowledge Hub API')
-  .setVersion('1.0')
-  .build();
+    .setTitle('Knowledge Hub')
+    .setDescription(' Knowledge Hub API')
+    .setVersion('1.0')
+    .build();
 
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('doc', app, documentFactory);
 
+  app.useGlobalInterceptors(new LoggingInterceptor());
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
