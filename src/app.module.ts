ce2110fd7f1bed65a8f56674b8da83850a9b252e
y-v-h <gyrinovich@gmail.com>
2026-04-06 00:30:47 +0200

    feat: imports all modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './users/user.modules';
+import { ArticleModule } from './articles/article.module';
+import { CategoryModule } from './categories/category.modules';
+import { CommentModule } from './comments/comment.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, ArticleModule, CategoryModule, CommentModule],
 })
 export class AppModule {}
