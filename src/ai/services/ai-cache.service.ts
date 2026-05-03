9e3f8ee35d216b8da634deac7839f997c7f2966a
y-v-h <gyrinovich@gmail.com>
2026-05-03 22:20:38 +0200

    refactor: remove unnecessary  console logs

--- src/ai/services/ai-cache.service.ts
+++ src/ai/services/ai-cache.service.ts
@@ -14,9 +14,7 @@ export class AiCacheService {
   >();
   constructor(private readonly configService: ConfigService) {}
   get(key: string): unknown | undefined {
-    console.log('[CACHE] get called with key:', key);
     const data = this.cache.get(key);
-    console.log('[CACHE] data:', data);
     if (!data) return undefined;
     if (data.expiresAt < Date.now()) {
       this.cache.delete(key);
@@ -26,9 +24,7 @@ export class AiCacheService {
   }
 
   set({ key, value }: AiCacheServiceSet): void {
-    console.log('[CACHE] set called with key:', key);
     const ttlSec = Number(this.configService.get('AI_CACHE_TTL_SEC')) || 300;
-    console.log('[CACHE] ttl from config:', ttlSec);
     const expiresAt = Date.now() + ttlSec * 1000;
     this.cache.set(key, { value, expiresAt });
   }
